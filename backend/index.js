9b7110b2702ce8c330f446d930399a4bbbea6836
IT21482502 <it21482502@my.sliit.lk>
2024-03-24 20:54:26 +0530

    Question server calling path added and middlewares part updated.

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,33 @@
-const express = require('express')
-require('dotenv').config()
-const cors = require('cors')
-const {db} = require('./db/db')
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const {db} = require('./db/db');
+const bodyParser = require('body-parser');
 
-//const routes = require('./routes/routes')
 
 const app = express()
 //console.log("Backend connected")
+
+
+
+/*---------- MiddleWares ------------*/
+
+app.use(express.json());
+app.use(bodyParser.json()); // Parse JSON bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
+
+
+
+
+/*---------- Kisal ------------*/
+const questionRoutes = require('./routes/questionsRoutes');
+app.use('/api',questionRoutes);
+
+app.use('/question', questionRoutes);
+
+
+
+
 
 
 /*---------- Connection with PORT ------------*/
@@ -14,7 +35,7 @@ const app = express()
 const server = () => {
     db();
     app.listen(process.env.PORT, () => {
-      console.log("listening to port ", process.env.PORT);
+      console.log("Server is running on port ", process.env.PORT);
     });
   };
   
