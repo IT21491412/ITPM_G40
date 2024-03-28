a5481efc856d0df792917ac0812c579c96d30941
IT21482502 <it21482502@my.sliit.lk>
2024-03-28 23:26:31 +0530

    answers model added and question model updated

--- backend/models/questions.js
+++ backend/models/questions.js
@@ -1,5 +1,23 @@
 const mongoose = require('mongoose');
 
+
+// DEFINE ANSWERS MODEL - ARRAY
+const answerSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: true 
+  },
+  answer: { 
+    type: String, 
+    required: true 
+  }
+});
+
+//END OF ANSWERS MODEL
+
+//===============================================================================================
+
+// DEFINE QUESTION MODEL
 const questionSchema = new mongoose.Schema({
     title: {
         type: String,
@@ -13,6 +31,7 @@ const questionSchema = new mongoose.Schema({
         type: String,
         required: true,
       },
+      answers: [answerSchema] // Array of answers associated with the question
 });
 
 const Question = mongoose.model('Question',questionSchema);
